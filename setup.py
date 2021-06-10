9b24d968202988d3b6b1c0e6705b260e6a616edd
David Emms <david_emms@hotmail.com>
2021-06-10 17:22:30 +0100

    Handle relative paths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SHOOT',
-    version='0.1.2',
+    version='0.1.3',
     long_description=__doc__,
     packages=['shootbio'],
     include_package_data=True,
