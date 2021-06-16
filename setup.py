749ebd9566c7af16a2a907e87b9174305df36a91
David Emms <david_emms@hotmail.com>
2021-06-16 16:16:47 +0100

    bump version: 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SHOOT',
-    version='0.2.1',
+    version='0.2.2',
     long_description=__doc__,
     packages=['shootbio'],
     include_package_data=True,
