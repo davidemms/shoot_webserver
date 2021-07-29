a945b27e790c3407eab8293ba55b3bdb4dfbd345
David Emms <david_emms@hotmail.com>
2021-07-29 22:16:34 +0100

    Get gene links working for Fungi dataset

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SHOOT',
-    version='0.4.0',
+    version='0.4.5',
     long_description=__doc__,
     packages=['shootbio'],
     include_package_data=True,
