62124066256fc9d78a50d5aaaa3c4d7949c4faa9
David Emms <david_emms@hotmail.com>
2021-07-30 10:08:01 +0100

    Add favico

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SHOOT',
-    version='0.4.5',
+    version='0.4.6',
     long_description=__doc__,
     packages=['shootbio'],
     include_package_data=True,
