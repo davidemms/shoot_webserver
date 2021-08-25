43a2f5d864b9c8ac429b0db2671b2fbd9f5c993c
David Emms <david_emms@hotmail.com>
2021-08-25 10:11:07 +0100

    Add Bacteria, disable multiple submits

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SHOOT',
-    version='0.5.2',
+    version='0.5.9',
     long_description=__doc__,
     packages=['shootbio'],
     include_package_data=True,
